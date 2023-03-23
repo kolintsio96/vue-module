801220dd553c141fcf3b3aa95f01dc6bd7c9b7ea
vkolintso <vkolintso@itera-research.com>
2023-03-23 17:20:44 +0200

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    'process.env': {}
+    'process.env': process.env
   },
   build: {
     lib: {
