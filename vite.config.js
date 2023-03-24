1912e45674eb8312da7a6056c5bcaed159fd9d50
vkolintso <vkolintso@itera-research.com>
2023-03-24 11:57:18 +0200

    updated package.json

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,9 @@ export default defineConfig({
   build: {
     lib: {
       entry: fileURLToPath(new URL('src/index.js', import.meta.url)),
-      name: 'vueModule',
+      name: 'vue-module',
       // the proper extensions will be added
-      fileName: 'vueModule',
+      fileName: 'vue-module',
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled  // into your library  external: ['vue'],  output: {
